5f276b4fc3beae1ffcbd120932ceeee819d501f0
Aroma-coder <j35978630@gmail.com>
2024-11-21 20:28:07 +0500

    Add files via upload

---
+++ kbhit().cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	//this is only to clear the concept of kbhit()
+	//when you press any key it will stop
+	while(!kbhit())
+	cout<<"Aroma-Coder\t";
+	return 0;
+}
